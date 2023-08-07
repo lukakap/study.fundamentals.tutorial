85caad6dec8d4490870753a3d2ec0ecf857f8068
admin <lkapa18@freeuni.edu.ge>
2023-08-07 13:33:21 +0400

    add bank account classes

---
+++ fundamentals.tutorial/BankAccount.cs
@@ -0,0 +1,80 @@
+﻿using System;
+namespace fundamentals.tutorial;
+
+public class BankAccount
+{
+    private static int s_accountNumberSeed = 1234567890;
+    private List<Transaction> _allTransactions = new List<Transaction>();
+
+    public string? Number { get; }
+    public string? Owner { get; set; }
+    public decimal Balance
+    {
+        get
+        {
+            decimal balance = 0;
+            foreach (var item in _allTransactions)
+            {
+                balance += item.Amount;
+            }
+
+            return balance;
+        }
+    }
+
+    public BankAccount(string name, decimal initialBalance)
+    {
+        Number = s_accountNumberSeed.ToString();
+        s_accountNumberSeed++;
+
+        Owner = name;
+        MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
+    }
+
+    public void MakeDeposit(decimal amount, DateTime date, string note)
+    {
+        if (amount <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+        }
+        var deposit = new Transaction(amount, date, note);
+        _allTransactions.Add(deposit);
+    }
+
+    public void MakeWithdrawal(decimal amount, DateTime date, string note)
+    {
+        if (amount <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+        }
+        if (Balance - amount < 0)
+        {
+            throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+        }
+        var withdrawal = new Transaction(-amount, date, note);
+        _allTransactions.Add(withdrawal);
+    }
+
+    public string GetAccountHistory()
+    {
+        /*
+         The history uses the StringBuilder class to format a string that contains one line for each transaction.
+        You've seen the string formatting code earlier in these tutorials. One new character is \t. That inserts a
+        tab to format the output. 
+         */
+
+        var report = new System.Text.StringBuilder();
+
+        decimal balance = 0;
+        report.AppendLine("Date\t\tAmount\tBalance\tNote");
+        foreach (var item in _allTransactions)
+        {
+            balance += item.Amount;
+            report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t{balance}\t{item.Notes}");
+        }
+
+        return report.ToString();
+    }
+}
+
+
